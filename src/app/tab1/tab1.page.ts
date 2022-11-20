ebe951435a4bafe3fbb2ae752d50d4ebdd992d83
Vitor Lopes <vitorlopesfranchi@gmail.com>
2022-11-20 10:00:52 -0300

    first commitr

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,110 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonModal, ModalController } from '@ionic/angular';
+import { take } from 'rxjs';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  constructor(private http: HttpClient, public modalController: ModalController) { }
+  presentingElement: any = null;
+  ngOnInit(): void {
+    this.refresh();
+    this.presentingElement = document.querySelector('.ion-page');
+
+  }
+  labs: any = [];
+  // Typically referenced to your ion-router-outlet
+
+  @ViewChild(IonModal) modal!: IonModal;
+
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name!: string;
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+  action: string = ''; 
+  currentLab: any = {};
+  openModal(lab: any, action: string) {
+    this.action = action;
+    this.currentLab = lab;
+    this.modal.present();
+  }
+  refresh() {
+    this.getLabs().pipe(
+      take(1)
+    ).subscribe((data: any) => {
+
+      this.labs = data;
+      console.log(this.labs);
+    });
+  }
+
+  editar() {
+    this.updateLab(this.currentLab).pipe(
+      take(1)
+    ).subscribe((data: any) => {
+      this.refresh();
+      this.modal.dismiss(null, 'cancel');
+    });
+  }
+
+  apagar() {
+    this.deleteLab(this.currentLab).pipe(
+      take(1)
+    ).subscribe((data: any) => {
+      this.refresh();
+      this.modal.dismiss(null, 'cancel');
+    });
+  }
+
+  doAction() {
+    if (this.action == 'Editar') {
+      this.editar();
+    } else {
+      this.adicionar();
+    }
+  }
+  
+  adicionar() {
+    this.createLab(this.currentLab).pipe(
+      take(1)
+    ).subscribe((data: any) => {
+      this.refresh();
+      this.modal.dismiss(null, 'cancel');
+    });
+  } 
+
+  getLabs() {
+    return this.http.get('https://fiapexercicio3.herokuapp.com/laboratorios/');
+  }
+
+  getLab(id: string) {
+    return this.http.get('https://fiapexercicio3.herokuapp.com/laboratorios/' + id);
+  }
+
+  createLab(lab: any) {
+    return this.http.post('https://fiapexercicio3.herokuapp.com/laboratorios/', lab);
+  }
+
+  updateLab(lab: any) {
+    return this.http.put('https://fiapexercicio3.herokuapp.com/laboratorios/' + lab.id, lab);
+  }
+
+  deleteLab(lab: any) {
+    return this.http.delete('https://fiapexercicio3.herokuapp.com/laboratorios/' + lab.id);
+  }
+
+  getLocalLabs() {
+    return JSON.parse(localStorage.getItem('labs')!) || [];
+  }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { ModalController, IonModal } from '@ionic/angular';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +10,112 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(private http: HttpClient, public modalController: ModalController) { }
+  presentingElement: any = null;
+  pickerController: any = null;
+  ngOnInit(): void {
+    this.refresh();
+    this.presentingElement = document.querySelector('.ion-page');
+    this.pickerController = document.querySelector('ion-picker-controller');
+
+  }
+  meds: any = [];
+  labs: any = [];
+  action: string = ''; 
+  @ViewChild(IonModal) modal!: IonModal;
+
+  name!: string;
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  currentMed: any = {};
+  openModal(med: any, action: string) {
+    this.refresh()
+    this.action = action;
+    this.currentMed = med;
+    this.modal.present();
+  }
+  refresh() {
+    this.getMedicamentos().pipe(
+      take(1)
+    ).subscribe((data: any) => {
+      this.meds = data; 
+    });
+    
+    this.getLabs().pipe(
+      take(1)
+    ).subscribe((data: any) => {
+      this.labs = data;
+    });
+  }
+
+  doAction() {
+    if (this.action == 'Editar') {
+      this.editar();
+    } else {
+      this.adicionar();
+    }
+  }
+
+  editar() {
+    this.updateMedicamento(this.currentMed).pipe(
+      take(1)
+    ).subscribe((data: any) => {
+      this.refresh();
+      this.modal.dismiss(null, 'cancel');
+    });
+  }
+
+  apagar() {
+    this.deleteMedicamento(this.currentMed).pipe(
+      take(1)
+    ).subscribe((data: any) => {
+      this.refresh();
+      this.modal.dismiss(null, 'cancel');
+    });
+  }
+
+  adicionar() {
+    this.createMedicamento(this.currentMed).pipe(
+      take(1)
+    ).subscribe((data: any) => {
+      this.refresh();
+      this.modal.dismiss(null, 'cancel');
+    });
+  } 
+
+  getMedicamentos() {
+    return this.http.get('https://fiapexercicio3.herokuapp.com/medicamentos/');
+  }
+
+  getMedicamento(id: string) {
+    return this.http.get('https://fiapexercicio3.herokuapp.com/medicamentos/' + id);
+  }
+
+  createMedicamento(medicamento: any) {
+    return this.http.post('https://fiapexercicio3.herokuapp.com/medicamentos/', medicamento);
+  }
+
+  updateMedicamento(medicamento: any) {
+    return this.http.put('https://fiapexercicio3.herokuapp.com/medicamentos/' + medicamento.id, medicamento);
+  }
+
+  deleteMedicamento(medicamento: any) {
+    return this.http.delete('https://fiapexercicio3.herokuapp.com/medicamentos/' + medicamento.id);
+  }
+
+  getLocalMedicamentos() {
+    return JSON.parse(localStorage.getItem('medicamentos') || '[]');
+  }
+
+  getLabs() {
+    return this.http.get('https://fiapexercicio3.herokuapp.com/laboratorios/');
+  }
 
 }
